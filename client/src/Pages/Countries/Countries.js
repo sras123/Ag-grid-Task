46990268419747c11093268347796e3b263adb21
sras123 <np03cs4s210054@heraldcollege.edu.np>
2023-07-01 16:01:58 +0545

    Modal pop up

--- client/src/Pages/Countries/Countries.js
+++ client/src/Pages/Countries/Countries.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './countries.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from '../../Components/Navbar';
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import axios from 'axios';
-
+import Modal from 'react-modal';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Countries = () => {
   const [rowData, setRowData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const handleRowClicked = (event) => {
+    setSelectedCountry(event.data);
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const columnDefs = [
     { field: 'name.common', headerName: 'Common Name' },
@@ -44,17 +55,49 @@ const Countries = () => {
   }, []);
 
   return (
-    <div>
-      <Navbar/>
-    <div className='ag-theme-alpine-dark' style={{height:500}}>
-      {loading ? (
+    <div className='content'>
+      <Navbar />
+      <div className='ag-theme-alpine-dark' style={{ height: 500 }}>
+        {loading ? (
           <div className='loading'>
             <h1>Loading the table. Wait...</h1>
-            </div>
+          </div>
         ) : (
-          <AgGridReact rowData={rowData} columnDefs={columnDefs} />
+          <AgGridReact rowData={rowData} columnDefs={columnDefs} onRowClicked={handleRowClicked} />
+
         )}
-    </div>
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        className="modal-container"
+      >
+        <h2 className='name'>Country Profile</h2>
+        {selectedCountry && (
+          <div className='modal-content'>
+
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div style={{ flex: 1 }}>
+                <h2 className='box'>Name</h2>
+                <h2 className='box'><strong>Capital</strong> </h2>
+                <h2 className='box'><strong>Region</strong></h2>
+                <h2 className='box'><strong>Languages</strong> </h2>
+              </div>
+              <div style={{ flex: 2, marginLeft: '10px' }} >
+                <h2 className='box-side'>{selectedCountry.name.common}</h2>
+                <h2 className='box-side'>{selectedCountry.capital}</h2>
+                <h2 className='box-side'>{selectedCountry.region}</h2>
+                <h2 className='box-side'>{Object.values(selectedCountry.languages).join(', ')}</h2>
+              </div>
+            </div>
+            <button onClick={closeModal} className="close-button">
+              <CloseIcon />
+            </button>
+          </div>
+
+        )}
+
+      </Modal>
     </div>
   )
 }
